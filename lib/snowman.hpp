e4b1b0567b3b9acf55d7ba3c169542633e9a1200
KeyboardFire <andy@keyboardfire.com>
2015-07-17 17:38:26 -0500

    release 0.1.0

--- lib/snowman.hpp
+++ lib/snowman.hpp
@@ -91,7 +91,7 @@ class Snowman {
         std::string debug();
         bool debugOutput;
         const static int MAJOR_VERSION = 0;
-        const static int MINOR_VERSION = 0;
+        const static int MINOR_VERSION = 1;
         const static int PATCH_VERSION = 0;
 };
 
