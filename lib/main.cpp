94414ea104e34538be5a86a5f67d1e25a472292a
KeyboardFire <andy@keyboardfire.com>
2015-07-24 15:39:22 -0500

    allow passing arguments like ./snowman -di

--- lib/main.cpp
+++ lib/main.cpp
@@ -16,72 +16,71 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i) {
         std::string arg(argv[i]);
         if (arg[0] == '-') {
-            char argid;
-            if (arg.length() >= 2 && arg[1] == '-') {
+            arg = arg.substr(1);
+            if (arg.length() >= 1 && arg[0] == '-') {
+                arg = arg.substr(1);
                 // no switch on strings :(
-                if (arg == "--help") argid = 'h';
-                else if (arg == "--interactive") argid = 'i';
-                else if (arg == "--debug") argid = 'd';
-                else if (arg == "--evaluate") argid = 'e';
-                else if (arg == "--minify") argid = 'm';
+                if (arg == "help") arg = "h";
+                else if (arg == "interactive") arg = "i";
+                else if (arg == "debug") arg = "d";
+                else if (arg == "evaluate") arg = "e";
+                else if (arg == "minify") arg = "m";
                 else {
                     std::cerr << "Unknown long argument `" << arg << "'" <<
                         std::endl;
                     return 1;
                 }
-            } else {
-                if (arg.length() != 2) {
-                    std::cerr << "Invalid syntax for argument `" << arg << "'"
-                        << std::endl;
-                    return 1;
-                } else {
-                    argid = arg[1];
-                }
             }
 
-            switch (argid) {
-            case 'h':
-                std::cout << "Usage: " << argv[0] << " [OPTION]... [FILENAME]\n" <<
-                    "Options:\n"
-                    "    -h, --help: (without filename) display this message\n"
-                    "    -i, --interactive: (without filename) start a REPL\n"
-                    "    -e, --evaluate: (without filename) takes one "
-                        "parameter, runs as Snowman code\n"
-                    "    -d, --debug: include debug output\n"
-                    "    -m, --minify: don't evaluate code; output minified "
-                        "version instead\n"
-                    "Snowman will read from STDIN if you do not specify a "
-                        "file name or the -h or -i options.\n"
-                    "Snowman version: " << VERSION_STRING << "\n";
-                return 0;
-            case 'i': {
-                std::cout << "Snowman REPL, " << VERSION_STRING <<
-                    std::endl;
-                std::cout << ">> ";
-                std::string line;
-                while (std::getline(std::cin, line)) {
-                    sm.run(line);
-                    std::cout << sm.debug();
+            for (char& argid : arg) {
+                switch (argid) {
+                case 'h':
+                    std::cout << "Usage: " << argv[0] << " [OPTION]... "
+                            "[FILENAME]\n" <<
+                        "Options:\n"
+                        "    -h, --help: (without filename) display this "
+                            "message\n"
+                        "    -i, --interactive: (without filename) start a "
+                            "REPL\n"
+                        "    -e, --evaluate: (without filename) takes one "
+                            "parameter, runs as Snowman code\n"
+                        "    -d, --debug: include debug output\n"
+                        "    -m, --minify: don't evaluate code; output "
+                            "minified version instead\n"
+                        "Snowman will read from STDIN if you do not specify a "
+                            "file name or the -h or -i options.\n"
+                        "Snowman version: " << VERSION_STRING << "\n";
+                    return 0;
+                case 'i': {
+                    std::cout << "Snowman REPL, " << VERSION_STRING <<
+                        std::endl;
                     std::cout << ">> ";
+                    std::string line;
+                    while (std::getline(std::cin, line)) {
+                        sm.run(line);
+                        std::cout << sm.debug();
+                        std::cout << ">> ";
+                    }
                 }
-            }
-            case 'd':
-                sm.debugOutput = true;
-                break;
-            case 'e':
-                if ((++i) == argc) {
-                    std::cerr << "Argument `" << arg << "' requires a"
-                        "parameter" << std::endl;
+                case 'd':
+                    sm.debugOutput = true;
+                    break;
+                case 'e':
+                    if ((++i) == argc) {
+                        std::cerr << "Argument `-e' requires a parameter" <<
+                            std::endl;
+                        return 1;
+                    }
+                    sm.run(std::string(argv[i]));
+                    return 0;
+                case 'm':
+                    minify = true;
+                    break;
+                default:
+                    std::cerr << "Unknown argument `-" << arg << "'" <<
+                        std::endl;
                     return 1;
                 }
-                sm.run(std::string(argv[i]));
-                return 0;
-            case 'm':
-                minify = true;
-                break;
-            default:
-                std::cerr << "Unknown argument `" << arg << "'" << std::endl;
-                return 1;
             }
         } else if (filename == "") {
             filename = arg;
